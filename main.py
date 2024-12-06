1956830a75d8e682d62cc6a46861250fc2fe9f11
Ethan Leatherwood <ethanleatherwood@mail.weber.edu>
2024-12-05 18:24:32 -0700

    attempt 1

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ if __name__ == "__main__":
     # TODO: Call the method to make the dog-specific sound
 
     # TODO print out all the animals
+
+    from animal import Animal
+    from dog import Dog
+
+    animal1 = Animal("Buddy", "Canine")
+    print(animal1)
+    animal1.speak()
+
+    dog1 = Dog("Buddy", "Canine", "Golden Retriever")
+    print(dog1)
+    dog1.speak()
+
+    print(Animal.all_animals)
+
